aac756730c38fad5ae65b314d00913c2f5faf64d
diego-sanchez-r <diegosanchezrojo@gmail.com>
2022-04-26 19:19:59 +0200

    Añadido perfil usuario

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -5,14 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Usuario;
 
 class UsuarioController extends AbstractController
 {
     #[Route('/usuario', name: 'app_usuario')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        //Comprobar si el usuario esta logeado.
+        if($this->getUser() === null){
+            return $this->redirectToRoute("login");
+        }
+        
+        $usuario = new Usuario();
+        $usuario = $this->getUser();
+        $repositorio = $doctrine->getRepository(Usuario::class);
+        $usuario = $repositorio->find($usuario->getId());
+        
         return $this->render('usuario/index.html.twig', [
             'controller_name' => 'UsuarioController',
+            'usuarioConectado' => $usuario,
         ]);
     }
 }
