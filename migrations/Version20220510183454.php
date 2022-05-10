e5a2e61f0fe9de8301a0454d4c4abfcbb8d641a0
diego-sanchez-r <diegosanchezrojo@gmail.com>
2022-05-10 20:36:29 +0200

    Creado el controlador de Noticias y migración BD

--- migrations/Version20220510183454.php
+++ migrations/Version20220510183454.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220502174348 extends AbstractMigration
+final class Version20220510183454 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,16 +20,14 @@ final class Version20220502174348 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE comentario DROP FOREIGN KEY FK_4B91E702DB38439E');
-        $this->addSql('ALTER TABLE comentario ADD CONSTRAINT FK_4B91E702DB38439E FOREIGN KEY (usuario_id) REFERENCES usuario (id)');
+        $this->addSql('CREATE TABLE noticias (id INT AUTO_INCREMENT NOT NULL, titulo VARCHAR(255) NOT NULL, descripcion VARCHAR(255) NOT NULL, fecha_creacion DATETIME NOT NULL, imagen VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE incidencia DROP INDEX averia_id, ADD UNIQUE INDEX UNIQ_C7C6728CD6F24FAE (averia_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE comentario DROP FOREIGN KEY FK_4B91E702DB38439E');
-        $this->addSql('ALTER TABLE comentario ADD CONSTRAINT FK_4B91E702DB38439E FOREIGN KEY (usuario_id) REFERENCES incidencia (id)');
+        $this->addSql('DROP TABLE noticias');
         $this->addSql('ALTER TABLE incidencia DROP INDEX UNIQ_C7C6728CD6F24FAE, ADD INDEX averia_id (averia_id)');
     }
 }
