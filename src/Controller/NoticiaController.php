6c5fa5ebf57f0636b26d1dae4287f1c70b03df59
diego-sanchez-r <diegosanchezrojo@gmail.com>
2022-05-11 13:39:59 +0200

    Página de noticias Bootstrap

--- src/Controller/NoticiaController.php
+++ src/Controller/NoticiaController.php
@@ -5,14 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Noticias;
 
 class NoticiaController extends AbstractController
 {
     #[Route('/noticia', name: 'app_noticia')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        
+        $repositorio = $doctrine->getRepository(Noticias::class);
+        $noticias = $repositorio->findBy(
+           [],
+           ["fechaCreacion" => "DESC"] 
+           );
         return $this->render('noticia/index.html.twig', [
-            'controller_name' => 'NoticiaController',
+            'noticias' => $noticias,
         ]);
     }
 }
